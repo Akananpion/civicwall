027e717bfd43649c60cc2a7cd71778d2dff5cfdf
Ntermast <gabynteramast@gmail.com>
2024-09-05 16:24:50 +0200

    users model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_countries.fields import CountryField
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+    phone_number = models.CharField(_('phone number'), max_length=15, blank=True, null=True)
+    country = CountryField(_('country'), blank=True, null=True)
+    region = models.CharField(_('region'), max_length=100, blank=True, null=True)
+    city = models.CharField(_('city'), max_length=100, blank=True, null=True)
+    profile_picture = models.ImageField(_('profile picture'), upload_to='profile_pics/', blank=True, null=True)
+    is_email_verified = models.BooleanField(_('email verified'), default=False)
+    is_phone_verified = models.BooleanField(_('phone verified'), default=False)
+
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=_('groups'),
+        blank=True,
+        help_text=_(
+            'The groups this user belongs to. A user will get all permissions '
+            'granted to each of their groups.'
+        ),
+        related_name="customuser_set",
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('user permissions'),
+        blank=True,
+        help_text=_('Specific permissions for this user.'),
+        related_name="customuser_set",
+        related_query_name="user",
+    )
+
+    def __str__(self):
+        return self.email
