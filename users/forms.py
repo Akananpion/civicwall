d2501e50ce86b4270ff62e5409232f001e700969
Ntermast <gabynteramast@gmail.com>
2024-09-05 20:25:13 +0200

    update user name creation

--- users/forms.py
+++ users/forms.py
@@ -8,6 +8,13 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         fields = ('email', 'first_name', 'last_name', 'phone_number', 'country', 'region', 'city')
 
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.username = self.cleaned_data['email']  # Set username to email
+        if commit:
+            user.save()
+        return user
+
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
