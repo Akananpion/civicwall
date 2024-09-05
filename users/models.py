88355611a2b968c6fc86cae5bd34c2c8c89e4d7d
Ntermast <gabynteramast@gmail.com>
2024-09-05 20:25:32 +0200

    set email as username

--- users/models.py
+++ users/models.py
@@ -33,5 +33,13 @@ class CustomUser(AbstractUser):
         related_query_name="user",
     )
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
     def __str__(self):
         return self.email
+
+    def save(self, *args, **kwargs):
+        if not self.username:
+            self.username = self.email
+        super().save(*args, **kwargs)
