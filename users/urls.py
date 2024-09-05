376fe1e2fd8633d06ce9e5a1a68952c5a91e38f9
Ntermast <gabynteramast@gmail.com>
2024-09-06 00:44:58 +0200

    update users urls

--- users/urls.py
+++ users/urls.py
@@ -8,13 +8,13 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('profile/', views.profile, name='profile'),
-    path('profile/picture/', views.update_profile_picture, name='update_profile_picture'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
-    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/picture/', views.update_profile_picture, name='update_profile_picture'),
 ]
 
 
