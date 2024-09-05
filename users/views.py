cf60efe4ef3eeb10783d251b321a357b1b4f27ba
Ntermast <gabynteramast@gmail.com>
2024-09-05 18:13:03 +0200

    activation function

--- users/views.py
+++ users/views.py
@@ -44,13 +44,40 @@ def register(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.is_active = False  # Deactivate account till it is confirmed
+            user.is_active = False
             user.save()
-            send_verification_email(request, user)
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your account.'
+            message = render_to_string('users/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                mail_subject, message, to=[to_email]
+            )
+            email.send()
             return render(request, 'users/account_activation_sent.html')
     else:
         form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = CustomUser.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, CustomUser.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.is_email_verified = True
+        user.save()
+        login(request, user)
+        return render(request, 'users/account_activation_complete.html')
+    else:
+        return render(request, 'users/account_activation_invalid.html')
 
 @login_required
 def profile(request):
