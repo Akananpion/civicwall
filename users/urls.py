c1db2ca2bf13dd274dce33d20c70c81a8e065efd
Ntermast <gabynteramast@gmail.com>
2024-09-08 01:47:08 +0200

    update login url

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    # path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', views.custom_login, name='login'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('logout/', views.custom_logout, name='logout'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
