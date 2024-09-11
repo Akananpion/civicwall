4bf5ff218e8d55c03efd503ca7b2ace078c74fbf
Paradoxe NGWASI <paradoxe.ngwasi@smatflow.com>
2024-09-11 01:02:40 +0000

    Update 4 files
    - /ansible/playbook.yml
    - /.gitignore
    - /smatflow_membership/urls.py
    - /smatflow_membership/settings.py

--- smatflow_membership/urls.py
+++ smatflow_membership/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
 from django.urls import path, include
 from django.views.i18n import set_language
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
     path('i18n/', include('django.conf.urls.i18n')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += i18n_patterns(
     path('', include('users.urls')),
